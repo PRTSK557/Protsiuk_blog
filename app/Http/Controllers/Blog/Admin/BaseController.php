86a61f26bbd54500ff2a50bd3a3f44f290fc0736
PRTSK557 <partyzan557@gmail.com>
2025-06-22 17:47:49 +0000

    Lab4

---
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Http\Controllers\Blog\BaseController as GuestBaseController;
+
+abstract class BaseController extends GuestBaseController
+{
+    // Тут можна розширювати спільну логіку для адмін-контролерів
+}
